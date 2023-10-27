d14d4faa365e07f835f9a5e5f1b10035b5720ede
Ivasik <iKuprienko670@gmail.com>
2023-10-27 17:25:51 +0300

    Home work

---
+++ 1 work.py
@@ -0,0 +1,8 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+num3 = float(input("Введите третье число: "))
+num4 = float(input("Введите четвертое число: "))
+
+average = (num1 + num2 + num3 + num4) / 4
+
+print(f"Среднее арифметическое: {average}")
